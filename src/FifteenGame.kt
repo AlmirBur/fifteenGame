ddbbbf911124b11b6fed242886e57983feb304f0
AlmirBur <31883981+AlmirBur@users.noreply.github.com>
2018-12-21 15:20:55 +0300

    Initial commit

---
+++ src/FifteenGame.kt
@@ -0,0 +1,169 @@
+import java.util.*
+
+fun createRandomMatrix(height: Int, width: Int): Matrix<Int> {
+    val num = height * width
+    val matrix = MatrixImpl(height, width, 0)
+    val list = (0 until num).toMutableList()
+    var i = 0
+    while (list.isNotEmpty()) {
+        val value = Random().nextInt(num)
+        if (value in list) {
+            matrix[i / width, i % width] = value
+            list.remove(value)
+            i++
+        }
+    }
+    return matrix
+}
+
+fun fifteenGameMoves(matrix: Matrix<Int>, moves: List<Int>): Matrix<Int> {
+    val M = MatrixImpl(matrix.height, matrix.width, 0)
+    for (i in 0 until M.height) for (j in 0 until M.width) { M[i, j] = matrix[i, j]; M[matrix[i, j]] = Cell(i, j) }
+    val num = M.height * M.width
+    for (i in 0 until moves.size) {
+        if (moves[i] !in 1 until num) throw IllegalStateException()
+        else {
+            val deltaColumn = Math.abs(M[moves[i]].column - M[0].column)
+            val deltaRow = Math.abs(M[moves[i]].row - M[0].row)
+            if (deltaColumn == 1 && deltaRow == 0 || deltaRow == 1 && deltaColumn == 0) {
+                M[M[0]] = moves[i]
+                M[M[moves[i]]] = 0
+                val x = M[0]
+                M[0] = M[moves[i]]
+                M[moves[i]] = x
+            }
+            else throw IllegalStateException()
+        }
+        printlnMatrix(M)
+    }
+    return M
+}
+
+fun fifteenGameSolution(matrix: Matrix<Int>): List<Int> {
+    val stepsLeftDown = listOf(1, 2, 2, 3, 0)
+    val stepsLeftUp = listOf(3, 2, 2, 1, 0)
+    val stepsUpRight = listOf(0, 3, 3, 2, 1)
+    val stepRightDown = listOf(1, 0, 0, 3, 2)
+    val stepRightUp = listOf(3, 0, 0, 1, 2)
+    var trajectory = listOf<Int>()
+    var tempSteps: List<Int>
+    val M = MatrixImpl(matrix.height, matrix.width, 0)
+    for (i in 0 until M.height)
+        for (j in 0 until M.width) {
+            M[i, j] = matrix[i, j]
+            M[matrix[i, j]] = Cell(i, j)
+        }
+    for (l in 0 until matrix.height - 2) {
+        println(l)
+        trajectory += M.change(List(M[0].row - l) { 3 } + List(M[0].column) { 2 })
+        for (k in l * M.width + 1..(l + 1) * M.width) {
+            if (M[k].column < (k - 1) % M.width) {
+                trajectory += M.change(List(M[k].row - M[0].row) { 1 } + List(M[0].column - M[k].column) { 2 })
+                tempSteps = listOf()
+                if (M[k].row == l + 1) {
+                    for (i in 1..(k - 1) % M.width - M[k].column) tempSteps += stepRightDown
+                    trajectory += M.change(tempSteps + listOf(1, 0))
+                } else {
+                    for (i in 1..(k - 1) % M.width - M[k].column) tempSteps += stepRightUp
+                    trajectory += M.change(tempSteps + listOf(3, 0))
+                }
+                trajectory += M.change(List(M[0].row - l) { 3 })
+            }
+            if (M[k].column != (k - 1) % M.width) {
+                trajectory += M.change(List(M[k].row - M[0].row) { 1 } +
+                        List(M[k].column - M[0].column) { 0 })
+                val plus = if (M[k].row == l) stepsLeftDown else stepsLeftUp
+                tempSteps = listOf()
+                for (i in k - l * M.width..M[k].column) tempSteps += plus
+                trajectory += M.change(tempSteps)
+            }
+            if (M[k].row != l && k % M.width != 0) {
+                trajectory += M.change(if (M[0].column > M[k].column) listOf(3, 2, 1)
+                else List(M[k].row - l) { 1 })
+                tempSteps = listOf()
+                for (i in 0 until M[k].row - l) tempSteps += stepsUpRight
+                trajectory += M.change(tempSteps + listOf(0, 3))
+            }
+        }
+        trajectory += M.change(listOf(1, 2, 2))
+        if (M[(l + 1) * M.width].row == l) continue
+        val deltaRow = M[(l + 1) * M.width].row - M[0].row
+        val deltaColumn = M[(l + 1) * M.width].column - M[0].column
+        val plus = List(deltaRow) { 1 } + List(deltaColumn) { 0 } + List(deltaRow) { 3 } + List(deltaColumn) { 2 }
+        tempSteps = listOf()
+        for (k in 1..deltaRow + deltaColumn) tempSteps += plus
+        trajectory += M.change(tempSteps + listOf(3, 0, 1, 0, 3, 2, 2, 1))
+    }
+    trajectory += M.change(List(M[0].column) { 2 })
+    for (l in 0 until M.width - 2) {
+        var k = M.width * (M.height - 1) + 1 + l
+        for (j in 0..1) {
+            if (M[k].column < j + l) trajectory += M.change(listOf(1, 2, 3, 0, 1, 0, 3, 2, 2, 1, 0, 3))
+            if (M[k].column != j + l) {
+                trajectory += M.change(List(M[k].row - M[0].row) { 1 } +
+                        List(M[k].column - M[0].column) { 0 })
+                val plus = if (M[k].row == M.height - 2) stepsLeftDown else stepsLeftUp
+                tempSteps = listOf<Int>()
+                for (i in j + l until M[k].column) tempSteps += plus
+                trajectory += M.change(tempSteps)
+            }
+            if (M[k].row == M.height - 1) {
+                trajectory += M.change(if (M[0].column > M[k].column) listOf(3, 2, 1, 0, 3)
+                else listOf(1, 0, 3))
+            }
+            k -= M.width
+        }
+        trajectory += M.change(stepsLeftDown)
+    }
+    while (M[M.width * M.height - 1] != Cell(M.height - 1, M.width - 1))
+        trajectory += M.change(listOf(0, 1, 2, 3))
+    trajectory += M.change(if (M[M.height - 1, M.width - 2] > M[M.height - 2, M.width - 1])
+        listOf(1, 2, 3, 0, 1, 0, 3, 2, 2, 1, 0, 0, 3, 2, 1, 0) else listOf(1, 0))
+    return trajectory
+}
+
+fun MatrixImpl<Int>.change(moves: List<Int>): MutableList<Int> {
+    val trajectory = mutableListOf<Int>()
+    for (i in 0 until moves.size) {
+        var dColumn = 0
+        var dRow = 0
+        when (moves[i]) {
+            0 -> dColumn = 1  //right
+            1 -> dRow = 1     //down
+            2 -> dColumn = -1 //left
+            3 -> dRow = -1    //up
+            else -> throw IllegalStateException()
+        }
+        if (this[0].row + dRow !in 0 until this.height || this[0].column + dColumn !in 0 until this.width)
+            throw IllegalStateException()
+        val digit = this[this[0].row + dRow, this[0].column + dColumn]
+        trajectory.add(digit)
+        this[this[0]] = digit
+        this[this[digit]] = 0
+        this[0] = this[digit]
+        this[digit] = Cell(this[0].row - dRow, this[0].column - dColumn)
+    }
+    return trajectory
+}
+
+fun main(args: Array<String>) {
+    val m = createRandomMatrix(24, 43)
+    fifteenGameMoves(m, fifteenGameSolution(m))
+}
+
+fun twoStr(n: Int) = when (n) {
+    0 -> "  ██"
+    in 1..9 -> "   $n"
+    in 10..99 -> "  $n"
+    in 100..999 -> " $n"
+    else -> throw IllegalArgumentException("num of digits is more then 1000")
+}
+
+fun printlnMatrix(matrix: Matrix<Int>) {
+    for (j in 0 until matrix.height) {
+        for (k in 0 until matrix.width) print(twoStr(matrix[j, k]))
+        println("")
+        println("")
+    }
+    readLine()
+}
---
+++ src/Matrix.kt
@@ -0,0 +1,70 @@
+data class Cell(val row: Int, val column: Int)
+
+interface Matrix<E> {
+    val height: Int
+    val width: Int
+
+    operator fun get(row: Int, column: Int): E
+    operator fun get(cell: Cell): E
+
+    operator fun set(row: Int, column: Int, value: E)
+    operator fun set(cell: Cell, value: E)
+}
+
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> =
+        if (height < 1 || width < 1) throw IllegalArgumentException()
+        else MatrixImpl(height, width, e)
+
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
+    private val elements = mutableMapOf<Cell, E>()
+
+    private val keys = MutableList(this.height * this.width) { Cell(0, 0) }
+
+    init { for (i in 0 until height) for (j in 0 until width) elements[Cell(i, j)] = e }
+
+    override fun get(row: Int, column: Int): E = get(Cell(row, column))
+
+    override fun get(cell: Cell): E = elements[cell] ?: throw IllegalArgumentException()
+
+    operator fun get(index: Int): Cell = keys[index]
+
+    override fun set(row: Int, column: Int, value: E) {
+        if (row !in 0 until height || column !in 0 until width) throw IllegalArgumentException()
+        else elements[Cell(row, column)] = value
+    }
+
+    override fun set(cell: Cell, value: E) = set(cell.row, cell.column, value)
+
+    operator fun set(index: Int, element: Cell) { keys[index] = element }
+
+    override fun equals(other: Any?) = if (other is Matrix<*> && height == other.height && width == other.width) {
+        var result = true
+        for (i in 0 until height) for (j in 0 until width) if (other[i, j] != this[i, j]) {result = false; break}
+        result
+    }
+    else false
+
+
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        result = 31 * result + elements.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        val res = mutableListOf<String>()
+        res.add("[ ")
+        for (row in 0 until height) {
+            res.add("[")
+            for (column in 0 until width) {
+                res.add(this[row, column].toString())
+                res.add(",")
+            }
+            res.removeAt(res.size - 1)
+            res.add("] ")
+        }
+        res.add("]")
+        return res.joinToString("")
+    }
+}
